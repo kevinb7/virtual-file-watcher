d28c75e6abafacd4df01f78f2633aa40c63d96c8
Kevin Barabash <kevinb7@gmail.com>
2015-07-12 23:53:31 -0600

    fixed bug listeining to 'change' events, implemented single run behaviour via options.once

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ var sourcePaths = [
     'example/src/test2.js'
 ];
 
-watch(sourcePaths, { debug: true })
+watch(sourcePaths, { debug: true, once: false })
     .transform(compile({ regex: /^example\/src/ }))
     .transform(concat(sourcePaths, "example/build/bundle.js"))
     .transform(log)
     .transform(write)
-    .take(1)
     .end();
 
 // TODO: run tests
-// TODO: quit if anything errors when options.once = true;
