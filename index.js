63f3fa331734f70417eee32930386c202483d5ce
Kevin Barabash <kevinb7@gmail.com>
2015-07-13 00:52:03 -0600

    added minification

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var watch = require('./lib/watch.js');
+var File = require('./lib/file.js');
 
 var actions = require('./lib/actions.js');
 var compile = actions.compile;
 var concat = actions.concat;
 var log = actions.log;
 var write = actions.write;
+var minify = actions.minify;
 
 var babel = require('babel-core');
 
@@ -18,9 +20,14 @@ watch(sourcePaths, { debug: true, once: false })
         compiler: function (code) { return babel.transform(code).code },
         regex: /^example\/src/
     }))
-    .transform(concat(sourcePaths, "example/build/bundle.js"))
+    .transform(concat(sourcePaths, 'example/build/bundle.js'))
+    .transform(write)
+    .transform(log)
+    .transform(minify)
     .transform(log)
     .transform(write)
     .end();
 
 // TODO: run tests
+// TODO: add newer... check all sources including deps
+// TODO: make reporting compiler errors more general, try this with TypeScript
